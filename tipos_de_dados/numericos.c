b2f58e1e3598a500564f992981011bf3d6909fa3
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 12:16:21 -0300

    tentativa de fazer uma calculadora de imc

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -7,7 +7,12 @@ int main(int argc, char* argv[]){
     double peso = 72.56;
     printf("Digite a idade: ");
     scanf("%d", &idade);
-    printf("A idade eh: %d\n", idade); 
+    printf("Digite o peso: ");
+    scanf("%.2f", &peso);
+    printf("Digite a altura: ");
+    scanf("%.2f", &altura);
+
+     printf("A idade eh: %d\n", idade); 
     printf("A altura eh: %.2f\n", altura);
     printf ("O peso eh: %.2f\n", peso);
     return 0;
