1f8168a1aa4dd5fe27b541f3e35b9a58c342429b
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 11:59:36 -0300

    add do \n

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -5,8 +5,8 @@ int main(int argc, char* argv[]){
     int idade = 18;
     float altura = 2.34;
     double peso = 102.56;
-    printf("A idade eh: %d", idade); 
-    printf("A altura eh: %f", altura);
-    printf ("O peso eh: %d", peso);
+    printf("A idade eh: %d\n", idade); 
+    printf("A altura eh: %f\n", altura);
+    printf ("O peso eh: %d\n", peso);
     return 0;
 }
