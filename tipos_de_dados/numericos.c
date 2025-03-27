b7e34eb5086be5ec62867c9c04fb66e591c795e2
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 12:11:01 -0300

    caossss

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -5,7 +5,8 @@ int main(int argc, char* argv[]){
     int idade = 18;
     float altura = 2.34;
     double peso = 72.56;
-    scanf("Digite a idade: %d\n", &idade)
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
     printf("A idade eh: %d\n", idade); 
     printf("A altura eh: %.2f\n", altura);
     printf ("O peso eh: %.2f\n", peso);
