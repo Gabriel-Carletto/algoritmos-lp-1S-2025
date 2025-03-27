b8bbe4d370bbb38f98146657d5246fbb6870489b
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 12:30:58 -0300

    caos 4

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]){
     
     int idade;
-    float altura;
+    double altura;
     double peso;
     printf("Digite a idade: ");
     scanf("%d", &idade);
@@ -13,8 +13,13 @@ int main(int argc, char* argv[]){
     scanf("%f", &altura);
     
 
-     printf("A idade eh: %d\n", idade); 
+    printf("A idade eh: %d\n", idade); 
     printf("A altura eh: %.2f\n", altura);
     printf ("O peso eh: %.2f\n", peso);
+
+    float dobro_peso = peso * 2;
+    float IMC = peso / (altura * altura);
+    
+    printf("O seu valor de IMC eh: %.2f")
     return 0;
 }
