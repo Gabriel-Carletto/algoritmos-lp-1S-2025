cbcc6c3076046467b6b7300e42a8e67e344ec9e2
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 12:18:59 -0300

    caos

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -2,10 +2,11 @@
 
 int main(int argc, char* argv[]){
     
-    int idade = 18;
-    float altura = 2.34;
-    double peso = 72.56;
+    int idade ;
+    float altura ;
+    double peso ;
     printf("Digite a idade: ");
+    getchar
     scanf("%d", &idade);
     printf("Digite o peso: ");
     scanf("%.2f", &peso);
