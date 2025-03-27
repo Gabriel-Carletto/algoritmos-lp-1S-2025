f792cdc3d381d9a810aa8f2f7561d8896faf6e65
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-27 12:00:09 -0300

    alteração do peso

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]){
     
     int idade = 18;
     float altura = 2.34;
-    double peso = 102.56;
+    double peso = 72.56;
     printf("A idade eh: %d\n", idade); 
     printf("A altura eh: %f\n", altura);
     printf ("O peso eh: %d\n", peso);
