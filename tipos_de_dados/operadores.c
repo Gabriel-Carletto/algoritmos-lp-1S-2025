03fa8a58f634d95d09060f6e8c73a713ca2c426b
Gabriel-Carletto <gabriellanius@outlook.com>
2025-03-28 11:37:31 -0300

    Operadores

---
+++ tipos_de_dados/operadores.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+    float N1;
+    float N2;
+
+    printf("Por favor, digite o valor de N1: ");
+    scanf("%f", &N1);
+    printf("Por favor, digite o valor de N2: ");
+    scanf("%f", &N2);
+    
+    // Adição
+
+    // Subtração
+
+    // Multiplicação
+
+    // Divisão
+
+    // Potenciação
+
+    // Radiciação
+
+    // Resto da divisão
+
+    // Quociente da divisão
+
+    // Incremento
+
+    // Decremento
+
+
+
+    return 0;
+}
