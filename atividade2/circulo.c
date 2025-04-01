c5bd5898ccbfc04d943a30dc5b86d055e1031055
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-01 16:37:28 -0300

    circulo

--- atividade2/circulo.c
+++ atividade2/circulo.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char* argv[]){
 
+     // Cálculo da área de um Círculo:
+    // Suponha um círculo de raio 5cm
+         
+    float raio = 5;
+    float PI = 3.14;
+
+    float area_do_circulo_parte_1 = (raio * raio);
+    float area_do_circulo_parte_2 = (area_do_circulo_parte_1 * PI);
+    printf("A area do losango eh: %.2f", area_do_circulo_parte_2);
 
     return 0;
 }
