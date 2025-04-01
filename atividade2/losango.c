84f0e5fbee9b2defa16609c9d93ff92711f51ddb
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-01 16:32:19 -0300

    Losango

--- atividade2/losango.c
+++ atividade2/losango.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char* argv[]){
 
+    // Cálculo da área de um Losango:
+    // Suponha um Losango de diagonal maior 6cm e diagonal menor 4cm
+         
+    float Diagonal_Maior = 6;
+    float Diagonal_Menor = 4;
 
+    float area_do_Losango_parte_1 = (Diagonal_Maior * Diagonal_Menor);
+    float area_do_Losango_parte_2 = (area_do_Losango_parte_1 / 2);
+    printf("A area do losango eh: %.2f", area_do_Losango_parte_2);
+    
     return 0;
 }
