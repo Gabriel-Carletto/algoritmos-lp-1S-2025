32d5d64ff7afca37e95519c5c2810678d275211e
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-03 11:17:02 -0300

    correção de falhas

--- atividade2/trapezio.c
+++ atividade2/trapezio.c
@@ -25,10 +25,10 @@ int main(int argc, char* argv[]){
    
    
    
-    printf("O valor da base maior eh: %2.f/n", Base_Trapezio_maior);
-    printf("O valor da base menor eh: %2.f/n", base_trapezio_menor);
-    printf("O valor da altura eh: %2.f/n", Altura_Trapezio);
-    printf("A area do trapezio eh: %.2f", area_do_trapezio_parte_3);
+    printf("O valor da base maior eh: %2.f\n", Base_Trapezio_maior);
+    printf("O valor da base menor eh: %2.f\n", base_trapezio_menor);
+    printf("O valor da altura eh: %2.f\n", Altura_Trapezio);
+    printf("A area do trapezio eh: %.2f\n", area_do_trapezio_parte_3);
     
 
     return 0;
--- atividade2/triangulo.c
+++ atividade2/triangulo.c
@@ -19,9 +19,9 @@ int main(int argc, char* argv[]){
     float area_do_triangulo_parte_2 = (area_do_triangulo_parte_1 / 2);
     
     
-    printf("O valor da base do trangulo eh: %2.f/n", Base_Triangulo);
-    printf("O valor da altura do triangulo eh: %2.f/n", Altura_Triangulo);
-    printf("A area do triangulo eh: %.2f", area_do_triangulo_parte_2);
+    printf("O valor da base do trangulo eh: %2.f\n", Base_Triangulo);
+    printf("O valor da altura do triangulo eh: %2.f\n", Altura_Triangulo);
+    printf("A area do triangulo eh: %.2f\n", area_do_triangulo_parte_2);
     
     
     return 0;
