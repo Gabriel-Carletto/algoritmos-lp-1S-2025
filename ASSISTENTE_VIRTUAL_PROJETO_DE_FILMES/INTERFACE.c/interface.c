d5f138e684d5b68a31f43493a75b8c1203c7b876
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-04 11:19:46 -0300

    assistente virtual

---
+++ ASSISTENTE_VIRTUAL_PROJETO_DE_FILMES/INTERFACE.c/interface.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char* argv[]){
+
+    printf("_______________________________________________________________________________________");
+
+
+
+
+
+    return 0;
+}
