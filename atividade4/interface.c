f39fc572b4861076dd377a04e28c53e4bf7ce65b
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-10 12:17:58 -0300

    correção do código e adição de loop

--- atividade4/interface.c
+++ atividade4/interface.c
@@ -12,75 +12,89 @@ int main(int argc, char* argv[]) {
     int tipo_de_filme;
     int genero_calmo;
     int genero_intenso;
+    int opcao_final;
+        do{
+        printf("Se quiser assistir um filme calmo digite 8.\n");
+        printf("Se quiser assistir um filme intenso digite 9.\n");
+        printf("Digite o tipo de filme que voce quer assistir: ");
+        scanf("%d", &tipo_de_filme);
 
-    printf("Se quiser assistir um filme calmo digite 8.\n");
-    printf("Se quiser assistir um filme intenso digite 9.\n");
-    printf("Digite o tipo de filme que voce quer assistir: ");
-    scanf("%d", &tipo_de_filme);
-
-    if (tipo_de_filme == 9) {
-
-        printf("_________________________________________________________________________________\n");
-        printf("Se quiser assistir um filme de acao digite 1.\n");
-        printf("Se quiser assistir um filme de suspense digite 2.\n");
-        printf("Se quiser assistir um filme de terror digite 3.\n");
-        printf("Digite o genero: ");
-        scanf("%d", &genero_intenso);
-
-        if (genero_intenso == 1) {
+        if (tipo_de_filme == 9) {
 
             printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Acao! Recomendacoes:\n");
-            printf("Batman, Vingadores, Gladiador, Matrix, etc.\n");
-        } else if (genero_intenso == 2) {
+            printf("Se quiser assistir um filme de acao digite 1.\n");
+            printf("Se quiser assistir um filme de suspense digite 2.\n");
+            printf("Se quiser assistir um filme de terror digite 3.\n");
+            printf("Digite o genero: ");
+            scanf("%d", &genero_intenso);
 
-            printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Suspense! Recomendacoes:\n");
-            printf("O Silencio dos Inocentes, A Origem, Seven, etc.\n");
-        } else if (genero_intenso == 3) {
+            if (genero_intenso == 1) {
 
-            printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Terror! Recomendacoes:\n");
-            printf("O Exorcista, Invocacao do Mal, Psicose, etc.\n");
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Acao! Recomendacoes:\n");
+                printf("Batman, Vingadores, Gladiador, Matrix, etc.\n");
+            } else if (genero_intenso == 2) {
+
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Suspense! Recomendacoes:\n");
+                printf("O Silencio dos Inocentes, A Origem, Seven, etc.\n");
+            } else if (genero_intenso == 3) {
+
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Terror! Recomendacoes:\n");
+                printf("O Exorcista, Invocacao do Mal, Psicose, etc.\n");
+            } else {
+                printf("Genero invalido.\n");
+            }
+
+        } else if (tipo_de_filme == 8) {
+
+            printf("_________________________________________________________________________________\n");  
+            printf("Se quiser animacao digite 4.\n");
+            printf("Se quiser comedia digite 5.\n");
+            printf("Se quiser ficcao cientifica digite 6.\n");
+            printf("Digite o genero: ");
+            scanf("%d", &genero_calmo);
+
+            if (genero_calmo == 4) {
+
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Animacao! Recomendacoes:\n");
+                printf("Sonic, Minecraft, etc.\n");
+            } else if (genero_calmo == 5) {
+
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Comedia! Recomendacoes:\n");
+                printf("Gente Grande, Click, etc.\n");
+            } else if (genero_calmo == 6) {
+
+                printf("_________________________________________________________________________________\n");
+                printf("Voce selecionou Ficcao! Recomendacoes:\n");
+                printf("Interestelar, Maze Runner, Blade Runner, etc.\n");
+            } else {
+                printf("Genero invalido.\n");
+            }
+
         } else {
-            printf("Genero invalido.\n");
+            printf("Tipo de filme invalido.\n");
         }
+       
 
-    } else if (tipo_de_filme == 8) {
 
-        printf("_________________________________________________________________________________\n");  
-        printf("Se quiser animacao digite 4.\n");
-        printf("Se quiser comedia digite 5.\n");
-        printf("Se quiser ficcao cientifica digite 6.\n");
-        printf("Digite o genero: ");
-        scanf("%d", &genero_calmo);
-
-        if (genero_calmo == 4) {
-
-            printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Animacao! Recomendacoes:\n");
-            printf("Sonic, Minecraft, etc.\n");
-        } else if (genero_calmo == 5) {
-
-            printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Comedia! Recomendacoes:\n");
-            printf("Gente Grande, Click, etc.\n");
-        } else if (genero_calmo == 6) {
-
-            printf("_________________________________________________________________________________\n");
-            printf("Voce selecionou Ficcao! Recomendacoes:\n");
-            printf("Interestelar, Maze Runner, Blade Runner, etc.\n");
-        } else {
-            printf("Genero invalido.\n");
-        }
-
-    } else {
-        printf("Tipo de filme invalido.\n");
-    }
-    printf("_________________________________________________________________________________\n");
-    printf("Encerrando o processo de recomendacao de filmes, obrigado por utilizar minha interface!\n");
-    printf("                         ASS: G. L. C.\n");
-    printf("_________________________________________________________________________________\n");
-    return 0;
+        printf("Deseja escolher um novo filme?\n");
+        printf("Se sim digite 0 .\n");
+        printf("Se nao digite 1 .\n");
+        scanf("%d", &opcao_final);
+    
+    
 }
+        while (opcao_final == 0);
+        if (opcao_final == 1) {
+            printf("_________________________________________________________________________________\n");
+            printf("Encerrando o processo de recomendacao de filmes, obrigado por utilizar minha interface!\n");
+            printf("                         ASS: G. L. C.\n");
+            printf("_________________________________________________________________________________\n");
+            }
 
+        return 0;
+}
