f0450e17e71394a606dedb25bfe6dbce4634904c
Gabriel-Carletto <gabriellanius@outlook.com>
2025-04-11 10:57:51 -0300

    contadore de 0 a 100

---
+++ Vetores/contador.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+    int i;
+    
+    printf ("Programa de numeros de 1 a 100! \n");
+    
+    for (int i = 0; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+    
+    printf ("Fim do programa.\n");
+
+return 0;
+}
