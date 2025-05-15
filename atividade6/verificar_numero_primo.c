fe2f7a3607b76d5e249e61dd8ebcb063f17bb7b1
Gabriel-Carletto <gabriellanius@outlook.com>
2025-05-15 12:10:51 -0300

    atualização long long

--- atividade6/verificar_numero_primo.c
+++ atividade6/verificar_numero_primo.c
@@ -5,8 +5,8 @@
 
 int main(int argc, char* argv[]){
 
-    int Numeros = 0;
-    
+     long long int Numeros = 0;
+    int ehPrimo = 1;
 
     printf("Inicializando verificador de numeros primos, favor aguardar...\n");
 
@@ -21,25 +21,35 @@ int main(int argc, char* argv[]){
     // Utilização da função if para determinar se um número é primo ou não.
      
      if (Numeros <= 1){
-        printf("%d O seu numero nao eh primo\n", Numeros);
+        printf("%lld O seu numero nao eh primo\n", Numeros);
+        return 0;
      }
     
      else if (Numeros == 2){
-        printf("%d O numero 2 eh o unico par primo, logo seu numero eh primo!\n", Numeros);
+        printf("%lld O numero 2 eh o unico par primo, logo seu numero eh primo!\n", Numeros);
+        return 0;
      }
      
      else if (Numeros % 2 == 0){
-        printf("%d o seu numero eh par e maior do que 2, logo seu numero nao eh primo.\n", Numeros);
+        printf("%lld o seu numero eh par e maior do que 2, logo seu numero nao eh primo.\n", Numeros);
+        return 0; // adiciona um return 0 para evitar execuções desnecessárias.
      }
+     
+     
+     
      int Limite = sqrt(Numeros);
-     for (int i = 3; i <= Limite; i += 2) {
-        
-        if (Numeros % i == 0){
-            printf("%d o seu numero nao eh primo.", Numeros);
+     for (int i = 3; i <= Limite; i += 2) { 
+       // utilizei o (i += 2) ao invés do i++ para uma eficiencia maior e evitar iteraçoes desnecessárias.
+       if (Numeros % i == 0) {
+        ehPrimo = 0; // ao marcar o valor como 0, o programa identifica como nao primo.
+        break; // utilizando o break para sair do loop assim que encontrar um divisor.
        }
-        else{
-            printf("%d o seu numero eh primo!!!!!");
-        }
-     }
+}
+    if(ehPrimo) {
+        printf("%lld o seu numero eh primo!\n", Numeros);
+    }
+    else{
+        printf("%lld o seu numero nao eh primo.\n", Numeros);
+    }
     return 0;
 }
